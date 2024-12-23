bf371e49b301eb36d848a05180d4970c5c4988ed
Arash Joobandi <yspdev@gmail.com>
2024-12-23 16:04:47 +1100

    improver sys prompt

--- prompts.py
+++ prompts.py
@@ -1,17 +1,22 @@
 #TODO: do not hardcode powershell, the LLM should be able to determine which Terminal is being used
 def PROMPT_AIDER(task_description: str) -> str:
     return f"""You are an expert coding AI using a cli tool named aider
-The overall goal is to """+task_description+"""
-Do not write code. 
-Only give guidance and commands.
+The overall task description is:
+```
+
+"""+task_description+"""
+
+```
+Do not write nor read code yourself.
+You only produce actions and instructions to aider.
 Do not ask aider questions. You need to make decisions and assumptions yourself.
-You can give aider file structure and context using '/ls' and '/add <file>'.
 It is recommended to run /map first to get a sense of the project.
+You can give aider context using '/add <relative_path_to_file1> <relative_path_to_file2?> ...'
 The response should be in this JSON schema:
 {
     "progress": "one sentence update on progress so far",
     "thought": "one sentence rationale",
-    "action":  "/instruct <message>" | "/ls" | "/git <git command>" | "/add <file>" | "/finish" | "/run <powershell_command>" | "/map" | "/test",
+    "action":  "/instruct <message>" | "/ls" | "/git <git command>" | "/add <relative_path_to_file1> <relative_path_to_file2?> ..." | "/map" | "/help",
     "future": "one sentence prediction",
 }
 """
